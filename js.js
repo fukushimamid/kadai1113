af3a8b3c227909c69cf93561970e7425ba8b6f78
fukushimamid <77185731+fukushimamid@users.noreply.github.com>
2022-01-10 02:29:42 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,19 +1,38 @@
-var $slide = $(".slide")
-  .slick({
-    fade: true,
-    speed: 1500,
-    autoplaySpeed: 3000,
-    arrows: false,
-    autoplay: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true
+  $('.slide').slick({
+    centerMode: true,
+    centerPadding: '25%',
+    dots: true,
+    arrows: true,
+  });
+
+  $(function(){
+    var setImg = '#top_visual';
+    var fadeSpeed = 1000;
+    var switchDelay = 10000;
+  
+    $(setImg).children('img').css({opacity:'0'});
+    $(setImg + ' img:first').stop().animate({opacity:'1',zIndex:'20'},fadeSpeed);
+  
+    setInterval(function(){
+        $(setImg + ' :first-child').animate({opacity:'0'},fadeSpeed).next('img').animate({opacity:'1'},fadeSpeed).end().appendTo(setImg);
+    },switchDelay);
   });
 
 const modal = document.getElementById('modal_body');
 function modal_open() {
-　　modal.style.display = 'block';
+  modal.style.display = 'block';
 };
 function modal_close() {
-　　modal.style.display = 'none';
+  modal.style.display = 'none';
 };
+function icon_change(){
+  var menu_che = document.getElementById('menu-btn-check');
+
+  if (menu_che.checked) {
+    document.getElementById("menu_open").style.display = 'none';
+    document.getElementById("menu_close").style.display = 'block';
+  } else {
+    document.getElementById("menu_open").style.display = 'block';
+    document.getElementById("menu_close").style.display = 'none';
+  }
+}
